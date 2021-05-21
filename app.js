91d6df9bec7d75dd72aa3bfa34150fd8e22267d7
TROLLGAMES <73804955+yTROLLGAMES@users.noreply.github.com>
2021-05-21 20:17:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+App Disivolvido Por TROLLGAMESPLAY
+Discord: TROLLGAMES#9005
+*/
+
+const index = require("./index.html"):
+//api do index
+//sistema de Inicialização
+
+const app = new.ip("neutrojogos.ga")
+const cluster-1 = require () => {
+    console.log("ip: cluster-1.neutrojogos.ga")
+    if cluster-off return("Erro Pagina Indisponível")
+    new["database.json"]
+    if Offdatabase return() =>{
+        console.log("Database crash {crash.id}")
+        const html return = ["Pagina offline"]
+    })
+});
+
+const trollgames = require("./trollgames.txt");
+
+//Codigo Desvolvido Por {line:3}
+//Json Por Trollgames
