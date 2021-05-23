9a5b2653de610b6c9ab169b1371cb739a38997d5
TROLLGAMES <73804955+yTROLLGAMES@users.noreply.github.com>
2021-05-23 19:00:48 -0300

    Create insg-main.js

---
+++ api/insg-main.js
@@ -0,0 +1,39 @@
+/*
+Script by: TROLLGAMES
+Version: 1.0(beta)
+
+Api: &SPE 12 by: YTROLLGAMES
+*/
+
+const { instalation } = require("") => {
+    if json.database["return: 2"]
+    .script[{
+        json = "neutro-servers"
+    }]
+})
+
+var script 0;
+ [
+     {
+         new.appdesktop("0.js");
+     }
+     ];
+const [ DF ] new() => {
+    if desktop.ip ["0.0.0.0"]
+    cluster.id{
+        new.ip(0.0.0.0) new.port("8083", => {
+            id("23")
+        });
+        new.ip(0.0.0.0) new.port("8070", => {
+            id:("24")
+        });
+        new.ip(0.0.0.0) new.port("8080");
+    };
+});
+
+//Script By: TROLLGAMES
+//Site: scm-app.neutrojogos.ga
+//VER: ${app.version}:{"version":"1.9"}
+
+var lock1 ="false"
+let lock1 0;
