649a58963a31d0150661378485828aa3a84b07c0
TROLLGAMES <73804955+yTROLLGAMES@users.noreply.github.com>
2021-05-28 12:40:31 -0300

    Create cdn.js

---
+++ api/cdn.js
@@ -0,0 +1,32 @@
+/*
+CDN_ID: "NeutroJogos"
+TYPE: "cdn"
+IP: "v1.neutrocdn.ga"
+*/
+
+
+// Variáveis da api:
+var trollgames.IP = {get.adminCdnIP};
+var user_ip = {client.ip};
+var cdn = {v1.neutrojogos.ga};
+
+//cookie da cdn:
+const { cdn } = new.cookie();
+
+//Bloquear IP Para Usuários
+const { block_IP } = new.blockAllIPs() => {
+    if ip = {trollgames.IP} return("0");
+});
+
+//console gen api
+const ping return("user_ip") => {
+    console.log("O IP: {user_ip} Tentou Acessar a CDN");
+    if {trollgames.IP} = return("1") => {
+        console.log("TROLLGAMES acessou a cdn");
+    });
+});
+
+
+//By: TROLLGAMES:9005
+//IP: www.neutrojogos.ga
+//CDN: v1.neutrocdn.ga
